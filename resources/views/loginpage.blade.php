9cda4c4d9a60b36b9042b073293dc915cefb5ef3
Earl Jeff <earldalugdugan@gmail.com>
2022-11-08 13:06:30 +0800

    new file:   .editorconfig 	new file:   .env.example 	new file:   .gitattributes 	new file:   .gitignore 	new file:   README.md 	new file:   app/Console/Kernel.php 	new file:   app/Exceptions/Handler.php 	new file:   app/Http/Controllers/Controller.php 	new file:   app/Http/Kernel.php 	new file:   app/Http/Middleware/Authenticate.php 	new file:   app/Http/Middleware/EncryptCookies.php 	new file:   app/Http/Middleware/PreventRequestsDuringMaintenance.php 	new file:   app/Http/Middleware/RedirectIfAuthenticated.php 	new file:   app/Http/Middleware/TrimStrings.php 	new file:   app/Http/Middleware/TrustHosts.php 	new file:   app/Http/Middleware/TrustProxies.php 	new file:   app/Http/Middleware/ValidateSignature.php 	new file:   app/Http/Middleware/VerifyCsrfToken.php 	new file:   app/Models/User.php 	new file:   app/Providers/AppServiceProvider.php 	new file:   app/Providers/AuthServiceProvider.php 	new file:   app/Providers/BroadcastServiceProvider.php 	new file:   app/Providers/EventServiceProvider.php 	new file:   app/Providers/RouteServiceProvider.php 	new file:   artisan 	new file:   bootstrap/app.php 	new file:   bootstrap/cache/.gitignore 	new file:   composer.json 	new file:   composer.lock 	new file:   config/app.php 	new file:   config/auth.php 	new file:   config/broadcasting.php 	new file:   config/cache.php 	new file:   config/cors.php 	new file:   config/database.php 	new file:   config/filesystems.php 	new file:   config/hashing.php 	new file:   config/logging.php 	new file:   config/mail.php 	new file:   config/queue.php 	new file:   config/sanctum.php 	new file:   config/services.php 	new file:   config/session.php 	new file:   config/view.php 	new file:   database/.gitignore 	new file:   database/factories/UserFactory.php 	new file:   database/migrations/2014_10_12_000000_create_users_table.php 	new file:   database/migrations/2014_10_12_100000_create_password_resets_table.php 	new file:   database/migrations/2019_08_19_000000_create_failed_jobs_table.php 	new file:   database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php 	new file:   database/seeders/DatabaseSeeder.php 	new file:   lang/en/auth.php 	new file:   lang/en/pagination.php 	new file:   lang/en/passwords.php 	new file:   lang/en/validation.php 	new file:   package.json 	new file:   phpunit.xml     new file:   public/.htaccess 	new file:   public/favicon.ico 	new file:   public/index.php 	new file:   public/robots.txt 	new file:   resources/css/app.css 	new file:   resources/js/app.js 	new file:   resources/js/bootstrap.js 	new file:   resources/views/loginpage.blade.php 	new file:   resources/views/welcome.blade.php 	new file:   routes/api.php 	new file:   routes/channels.php 	new file:   routes/console.php 	new file:   routes/web.php 	new file:   storage/app/.gitignore 	new file:   storage/app/public/.gitignore 	new file:   storage/framework/.gitignore 	new file:   storage/framework/cache/.gitignore 	new file:   storage/framework/cache/data/.gitignore 	new file:   storage/framework/sessions/.gitignore 	new file:   storage/framework/testing/.gitignore 	new file:   storage/framework/views/.gitignore 	new file:   storage/logs/.gitignore 	new file:   tests/CreatesApplication.php 	new file:   tests/Feature/ExampleTest.php 	new file:   tests/TestCase.php 	new file:   tests/Unit/ExampleTest.php 	new file:   vite.config.js

---
+++ resources/views/loginpage.blade.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <div class="container">
+            <div class="row justify-content-center align-items-center mt-5">
+                <div class="col-md-12" style="height:250px; width:250px;">
+                   <?php
+                   echo "<img src='static\Kapit_Buhay_Logo.png'>";
+                   ?>
+                </div>
+            </div>
+            <div class="row justify-content-center align-items-center m-2">
+                <div class="col-md-6">
+                    <h1 class="text-center">Welcome</h1>
+                </div>
+            </div>
+            <div class="row justify-content-center align-items-center m-3">
+                <div class="border" style="height:300px; width:350px">
+                    <div class="row mt-5 no-gutters">
+                        <div class="col-md-5 text-end">Username</div>
+                        <div class="col-md-7">
+                            <input type="email" class="form-control">
+                        </div>
+                    </div>
+                    <div class="row mt-2">
+                        <div class="col-md-5 text-end">Password</div>
+                        <div class="col-md-7">
+                            <input type="password" class="form-control">
+                        </div>
+                    </div>
+                    <div class="row mt-5 mx-3">
+                        <div class="col-md-12">
+                        <button type="button" class="btn btn-light w-100">login</button>
+                        </div>
+                    </div>
+                    <div class="row mt-4 w-100 mx-1">
+                        <div class="col-md-12 text-center"><button type="button" class="btn btn-light w-50"   data-bs-toggle="modal" data-bs-target="#registerModal">Create Account</button></div>
+                    </div>
+                </div>
+            </div>
+            <div class="modal fade" id="registerModal" tabindex="-1" aria-labelledby="registerModalLabel" aria-hidden="true">
+                <div class="modal-dialog modal-lg p-5">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h1 class="modal-title fs-5" id="registerModalLabel">Registration</h1>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+
+                         </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
