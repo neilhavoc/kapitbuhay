429f0b9a62f7506fc4aaf1910f376a10abc06af2
elkapre <khefrey123@gmail.com>
2022-11-23 10:32:30 +0800

    MA - Police View

--- resources/views/layouts/manage_accounts.blade.php
+++ resources/views/layouts/manage_accounts.blade.php
@@ -4,8 +4,7 @@
 
 
     <!-- Styles -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    @include('components.AdminHeader')
     <script type="text/javascript" src="http://code.jquery.com/jquery-latest.js"></script>
     @yield('styles')
 
