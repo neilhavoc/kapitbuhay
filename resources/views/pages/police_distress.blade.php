394c412648dcca2880940f952d51972f8019a122
BhoyM <godwinjoshuacapalamendoza@gmail.com>
2022-12-23 13:45:16 +0800

    Updated_police&vaw_web

--- resources/views/pages/police_distress.blade.php
+++ resources/views/pages/police_distress.blade.php
@@ -68,105 +68,19 @@
                                 <td></td>
                                 <td></td>
                                 <td>
-                                    <button class="btn btn-warning" type="button" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
-                                        View
-                                    </button>
+                                <a role="button" class="btn btn-warning"  href="police_reviewdistressmessage">View</a>
                                 </td>
                             </tr>
                 </tbody>
     </div>
 </div>
-<div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-    <div class="modal-dialog modal-fullscreen">
-      <div class="modal-content">
-        <div class="modal-header">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Back</button>
-          <h3 class="modal-title position-absolute top-25 start-50 translate-middle" id="staticBackdropLabel">Review Distress Message</h3>
-        </div>
-        <div class="modal-body">
-            <div class="row mx-3" style="height: 100vh;">
-                <div class="col-md-6">
-                    <div class="row mt-5">
-                        <div class="col-md-4 fw-bold " >Reference ID:&nbsp;<label>sample</label></div>
-                        <div class="col-md-4 fw-bold ">Date:&nbsp;<label>sample</label></div>
-                        <div class="col-md-4 fw-bold ">Time:&nbsp;<label>sample</label></div>
-                    </div>
-                    <div class="row mt-3">
-                        <div class="col-md-6 fw-bold ">From:&nbsp;<label>sample</label></div>
-                        <div class="col-md-6 fw-bold ">User ID:&nbsp;<label>sample</label></div>
-                    </div>
-                    <div class="row mt-3">
-                        <div class="col-md-6 fw-bold ">Specific Location:&nbsp;<label>sample</label></div>
-                    </div>
-                    <div class="row mt-3">
-                        <div class="row">
-                            <div class="col-md-3 fw-bold ">Distress Message: </div>
-                        </div>
-                       <div class="row">
-                            <div class="col-md-12">  <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" disabled="disabled"></textarea></div>
-                       </div>
-                    </div>
-                    <div class="row mt-3 mx-5 mb-5 justify-content-between">
-                    <div class="form-check">
-                            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-                            <label class="form-check-label" for="flexCheckDefault">
-                            Verify
-                            </label>
-                            </div>
-                            <div class="form-check">
-                            <input class="form-check-input" type="checkbox" value="" id="flexDefault">
-                            <label class="form-check-label" for="flexCheckDefault">
-                            Not Verified
-                            </label>
-                            </div>
-                            <div class="row mt-1 mx-5 justify-content-between">
-                        <div class="col-md-8">
-                            <button type="button" class="btn btn-danger float-md-end" data-bs-toggle="modal" data-bs-target="#staticBackdrop2">Report User</button>
-                        </div>
-                    </div>
-                         
-                        </div>
 
-                
-                    <div class="row mt-5 mx-5 justify-content-evenly">
-                        <div class="col-md-6">
-                            <a role="button" class="btn btn-success"  href="police_createReports">Create Report</a>
-                        </div>
-                        <div class="col-md-6">
-                            <button type="button" class="btn btn-warning">Cancel</button>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-6 ">
-                    <div class="row mt-3 ">
-                        <div class="col-md-6 fw-bold ">Location Link:</div>
-                    </div>
-                    <div class="row mt-3">
-                        Sample
-                    </div>
-                    <div class="row mt-3 mx-3">
-                        <div id="googleMap" style="width:100%;height:400px; " ></div>
-                    </div>
-                </div>
-            </div>
+         
 
-          </div>
-
-              </div>
-            </div>
-          </div>
+          
 @stop
 
 <!-- Scripts -->
 @section('scripts')
-<script>
-    function myMap() {
-    var mapProp= {
-      center:new google.maps.LatLng(51.508742,-0.120850),
-      zoom:5,
-    };
-    var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
-    }
-</script>
-<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCnwBnZtVNK_pmkcdWB6BQeH9S9LQUlXew&callback=myMap"></script>
+
 @stop
